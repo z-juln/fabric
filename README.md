25fb0bd581870723f6ebbbd82b2c304457242056
juln <1850021148@qq.com>
2023-06-27 16:32:14 +0800

    feat: 1.0.0

---
+++ README.md
@@ -0,0 +1,70 @@
+# fabric代码规范
+
+一个包含 prettier，eslint，stylelint 的配置文件合集
+
+A collection of configuration files containing prettier, eslint, stylelint
+
+## Use
+
+安装
+
+```
+npm i @juln/fabric -D
+yarn add @juln/fabric -D
+```
+
+in .eslintrc.js
+
+```js
+module.exports = {
+  extends: ['@juln/fabric'],
+
+  rules: {
+    // your rules
+  },
+};
+```
+
+in .prettierrc.js
+
+```js
+const prettier = require('@juln/fabric/prettier');
+
+module.exports = {
+  ...prettier,
+};
+```
+
+in .stylelintrc.js
+
+```js
+module.exports = {
+  extends: [require.resolve('@juln/fabric/stylelint')],
+  rules: {
+    // your rules
+  },
+};
+```
+
+## 相关问题
+
+1. @typescript-eslint/no-unused-var 误报
+
+@typescript-eslint/no-unused-vars 规则在极少数情况下会误报。 比如下面这段代码, ts函数类型定义，函数中的参数会被当作真实变量被判断。
+
+```ts
+type Fn = (arg: any) => void; // arg 误报
+```
+
+解决方案: 使用`@typescript-eslint/no-unused-vars-experimental`代替`@typescript-eslint/no-unused-vars`。但是lint速度会很明显变慢很多
+
+```js
+// .eslintrc.js
+module.exports = {
+  extends: ['@juln/fabric'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars-experimental': 'error',
+  },
+};
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      // 不允许 return 语句出现在 global 环境下
+      globalReturn: false,
+      // 开启全局 script 模式
+      impliedStrict: true,
+    },
+  },
+  settings: {
+    //自动发现React的版本，从而进行规范react代码
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'prettier',
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+  ],
+  // 以当前目录为根目录，不再向上查找 .eslintrc.js
+  root: true,
+  rules: {
+    'no-console': 'off',
+    'prefer-spread': ['warn'],
+    'no-extra-semi': ['warn'],
+    'quotes': ['error', 'single'],
+    'linebreak-style': ['warn', 'unix'],
+    'comma-dangle': 'off',
+    'arrow-parens': 'off',
+    'no-alert': 'off',
+    'camelcase': 'off',
+    // 箭头函数的箭头前后必须有空格
+    'arrow-spacing': [
+      2,
+      {
+        before: true,
+        after: true,
+      },
+    ],
+    'init-declarations': 'off',
+    "no-unused-vars": "off",
+
+    // react 自定义规则
+    'react/jsx-uses-react': 2,
+    'react/no-did-update-set-state': 2,
+    'react/no-redundant-should-component-update': 2,
+    'react/no-will-update-set-state': 2,
+    'react/require-render-return': 2,
+    'react/jsx-closing-bracket-location': [2, { selfClosing: 'tag-aligned' }],
+    'react/jsx-key': 2,
+    'react/jsx-no-bind': 'off',
+    'react/jsx-pascal-case': 2,
+    'react/jsx-uses-vars': 2,
+    'react-hooks/rules-of-hooks': 'error',
+    'react/react-in-jsx-scope': 0,
+
+    /**
+     * 禁止使用 console
+     */
+    'no-console': 'off',
+    'prefer-spread': ['warn'],
+    'no-extra-semi': ['warn'],
+    'quotes': ['error', 'single'],
+    'linebreak-style': ['warn', 'unix'],
+    'comma-dangle': 'off',
+    'arrow-parens': 'off',
+    'no-alert': 'off',
+    'camelcase': 'off',
+    // 箭头函数的箭头前后必须有空格
+    'arrow-spacing': [
+      2,
+      {
+        before: true,
+        after: true,
+      },
+    ],
+
+    // 自定义ts规则
+    '@typescript-eslint/init-declarations': 'off',
+    /*
+    eslint no-unused-vars didn't work at typescript
+    https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    */
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "prettier/prettier": "error",
+  },
+};
