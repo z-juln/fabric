ba967eb70e28fbfee6ca116a4794e52402fcf1d8
juln <1850021148@qq.com>
2023-07-05 16:13:23 +0800

    fix: scss import

--- stylelint.js
+++ stylelint.js
@@ -1,32 +1,33 @@
-'use strict';
+"use strict";
 
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-css-modules',
-    'stylelint-config-prettier',
+    "stylelint-config-standard",
+    "stylelint-config-css-modules",
+    "stylelint-config-prettier",
   ],
-  plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  plugins: ["stylelint-declaration-block-no-ignored-properties"],
   rules: {
-    'no-descending-specificity': null,
-    'function-url-quotes': 'always',
-    'selector-attribute-quotes': 'always',
-    'selector-class-pattern': null,
-    'font-family-no-missing-generic-family-keyword': null, // iconfont
-    'plugin/declaration-block-no-ignored-properties': true,
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
-    'selector-type-no-unknown': null,
-    'value-keyword-case': ['lower', { ignoreProperties: ['composes'] }],
+    "import-notation": null,
+    "no-descending-specificity": null,
+    "function-url-quotes": "always",
+    "selector-attribute-quotes": "always",
+    "selector-class-pattern": null,
+    "font-family-no-missing-generic-family-keyword": null, // iconfont
+    "plugin/declaration-block-no-ignored-properties": true,
+    "unit-no-unknown": [true, { ignoreUnits: ["rpx"] }],
+    "selector-type-no-unknown": null,
+    "value-keyword-case": ["lower", { ignoreProperties: ["composes"] }],
   },
   overrides: [
     {
-      files: ['*.scss', '**/*.scss'],
-      customSyntax: 'postcss-scss',
+      files: ["*.scss", "**/*.scss"],
+      customSyntax: "postcss-scss",
     },
     {
-      files: ['*.less', '**/*.less'],
-      customSyntax: 'postcss-less',
+      files: ["*.less", "**/*.less"],
+      customSyntax: "postcss-less",
     },
   ],
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
 };
